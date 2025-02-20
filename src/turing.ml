cf977b98040e850b40d8c30f898b7f1807483333
thoo-ma <trobin@student.42.fr>
2025-02-20 18:50:08 +0000

    parsing

--- src/turing.ml
+++ src/turing.ml
@@ -18,30 +18,104 @@ type turing_machine = {
 	transitions : (string, transition list) Hashtbl.t;
 }
 
+let validate_alphabet alphabet_strings =
+  List.map (fun s ->
+    if String.length s <> 1 then
+      failwith (Printf.sprintf "Alphabet entry '%s' must be a single character" s)
+    else s.[0]
+  ) alphabet_strings
+
+let validate_blank blank_str alphabet =
+  let blank =
+    if String.length blank_str <> 1 then
+      failwith "Blank must be a single character"
+    else blank_str.[0]
+  in
+  if not (List.mem blank alphabet) then
+    failwith "Blank character must be part of the alphabet";
+  blank
+
+let validate_finals finals states =
+  List.iter (fun final ->
+    if not (List.mem final states) then
+      failwith (Printf.sprintf "Final state '%s' must be part of the states" final)
+  ) finals;
+  finals
+
+let validate_initial initial states =
+  if not (List.mem initial states) then
+    failwith (Printf.sprintf "initial state '%s' must be part of the states" initial)
+  else
+    initial
+
+let validate_transitions transitions_json states =
+  List.iter (fun (state, _) ->
+    if not (List.mem state states) then
+      failwith (Printf.sprintf "Transition '%s' must be part of the states" state)
+  ) transitions_json;
+  transitions_json
+
+let validate_read read alphabet =
+  if String.length read <> 1 then
+    failwith (Printf.sprintf "read value '%s' must be a single character" read);
+  let read_char = read.[0] in
+  if not (List.mem read_char alphabet) then
+    failwith (Printf.sprintf "read value '%c' must be part of the alphabet" read_char)
+  else
+    read_char
+
+let validate_write write alphabet =
+  if String.length write <> 1 then
+    failwith (Printf.sprintf "write value '%s' must be a single character" write);
+  let write_char = write.[0] in
+  if not (List.mem write_char alphabet) then
+    failwith (Printf.sprintf "write value '%c' must be part of the alphabet" write_char)
+  else
+    write_char
+
+let validate_state state states =
+  if not (List.mem state states) then
+    failwith (Printf.sprintf "state '%s' must be part of the states" state)
+  else
+    state
+
+let validate_action action =
+    if (action = "LEFT" || action = "RIGHT") then
+        action
+    else
+        failwith (Printf.sprintf "Action '%s' must be LEFT or RIGHT" action)
+
+let validate_input alphabet blank input =
+  String.iter (fun c ->
+    if not (List.mem c alphabet) then
+      failwith (Printf.sprintf "Input character '%c' must be part of the alphabet" c)
+    else if c = blank then
+      failwith (Printf.sprintf "Input cannot contain blank character '%c'" c)
+  ) input
+
 (* Parse a JSON description into a Turing machine type *)
 let parse_machine json =
-	let name = json |> member "name" |> to_string in
-	let alphabet = json |> member "alphabet" |> to_list |> filter_string |>
-		List.map (fun s -> s.[0]) in
-	let blank = json |> member "blank" |> to_string |> fun s -> s.[0] in
-	let states = json |> member "states" |> to_list |> filter_string in
-	let initial = json |> member "initial" |> to_string in
-	let finals = json |> member "finals" |> to_list |> filter_string in
+  let name = json |> member "name" |> to_string in
+  let alphabet = json |> member "alphabet" |> to_list |> filter_string |> validate_alphabet in
+  let blank = json |> member "blank" |> to_string |> fun b -> validate_blank b alphabet in
+  let states = json |> member "states" |> to_list |> filter_string in
+  let initial = json |> member "initial" |> to_string |> fun i -> validate_initial i states in
+  let finals = json |> member "finals" |> to_list |> filter_string |> fun f -> validate_finals f states in
 
-	(* Read transitions *)
-	let transitions = Hashtbl.create 10 in
-	let transitions_json = json |> member "transitions" |> to_assoc in
-	List.iter (fun (state, ts) ->
-		let ts_list = ts |> to_list |> List.map (fun t ->
-			{
-				read = t |> member "read" |> to_string |> (fun s -> s.[0]);
-				to_state = t |> member "to_state" |> to_string;
-				write = t |> member "write" |> to_string |> (fun s -> s.[0]);
-				action = t |> member "action" |> to_string;
-			}
-		) in
-		Hashtbl.add transitions state ts_list
-	) transitions_json;
+  (* Read transitions *)
+  let transitions = Hashtbl.create 10 in
+	let transitions_json = json |> member "transitions" |> to_assoc |> fun t -> validate_transitions t states in
+    List.iter (fun (state, ts) ->
+        let ts_list = ts |> to_list |> List.map (fun t ->
+            {
+                read = t |> member "read" |> to_string |> (fun r -> validate_read r alphabet);
+                to_state = t |> member "to_state" |> to_string |> (fun s -> validate_state s states);
+                write = t |> member "write" |> to_string |> (fun w -> validate_write w alphabet);
+                action = t |> member "action" |> to_string |> validate_action;
+            }
+        ) in
+        Hashtbl.add transitions state ts_list
+    ) transitions_json;
 
 	(* Construct the Turing machine record *)
 	{ name; alphabet; blank; states; initial; finals; transitions }
@@ -56,6 +130,9 @@ let display_transitions machine =
 
 (* Simulate the Turing machine *)
 let simulate_machine machine input =
+  (* Validate input before simulation *)
+  validate_input machine.alphabet machine.blank input;
+
 	(* Tape representation *)
 	let tape = Bytes.of_string input in					(* Converts the input string *)
 	let tape_length = Bytes.length tape in			(* Gets the length of the tape *)
