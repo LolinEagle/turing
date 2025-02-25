9ecb4f5b5750424d8feac24b5e56fd2a2b7a2b2d
thoo-ma <trobin@student.42.fr>
2025-02-25 21:08:09 +0000

    add test_success and test_failure handlers

--- test/02n.sh
+++ test/02n.sh
@@ -2,10 +2,12 @@
 
 TURING="../_build/default/src/main.exe"
 MACHINE="../machines/02n.json"
+SUCCESS="y"
+FAILURE="n"
 TOTAL=0
 PASSED=0
 
-test(){
+test() {
 	TOTAL=$((TOTAL + 1))
 	local input=$1
 	local expected=$2
@@ -18,19 +20,25 @@ test(){
 	fi
 }
 
-echo "02n valid cases _________________________________________________________"
-test "00" "y"
-test "0000" "y"
-test "00000000" "y"
-echo "02n invalid cases _______________________________________________________"
-test "0" "n"
-test "000" "n"
-test "00000" "n"
-test "000000" "n"
-test "0000000" "n"
-test "000000000" "n"
+test_success() {
+    test "$1" "$SUCCESS"
+}
+
+test_failure() {
+    test "$1" "$FAILURE"
+}
+
+echo "02n valid inputs ________________________________________________________"
+test_success "00"
+test_success "0000"
+test_success "00000000"
+
+echo "02n invalid inputs ______________________________________________________"
+test_failure "0"
+test_failure "000"
+test_failure "00000"
+test_failure "000000"
+test_failure "0000000"
+test_failure "000000000"
 
 echo "$PASSED/$TOTAL tests passed"
-
-# Exit with status 1 if any test failed
-# [ $PASSED -eq $TOTAL ] || exit 1
--- test/unary_sub.sh
+++ test/unary_sub.sh
@@ -5,7 +5,7 @@ MACHINE="../machines/unary_sub.json"
 TOTAL=0
 PASSED=0
 
-test(){
+test() {
 	TOTAL=$((TOTAL + 1))
 	local input=$1
 	local expected=$(echo "$2" | sed 's/\./\\./g')
@@ -26,6 +26,3 @@ test "11-1=" "1"
 test "111-11=" "1"
 test "1111-111=" "1"
 echo "$PASSED/$TOTAL tests passed"
-
-# Exit with status 1 if any test failed
-# [ $PASSED -eq $TOTAL ] || exit 1
